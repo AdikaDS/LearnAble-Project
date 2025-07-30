0fde4dd4ea5128fdfa680ae27db4ef9cde5cfd60
Adika Dwi Saputra <adika.dwi.saputra@mail.ugm.ac.id>
2025-07-30 14:42:26 +0700

    tanpa filter

--- machine-learning/backend-android/handlers/subject.py
+++ machine-learning/backend-android/handlers/subject.py
@@ -1,14 +1,11 @@
-import logging
-
 from flask import jsonify
-
 from services.firestore_service import db
-
+import logging
 
 def handle_subjects_by_level(level, req):
     logging.info("Mengambil pelajaran untuk jenjang %s", level)
     try:
-        docs = db.collection("subjects").where(filter=("schoolLevel", "==",level)).stream()
+        docs = db.collection("subjects").where("schoolLevel", "==", level).stream()
 
         chips = []
         for doc in docs:
@@ -19,39 +16,26 @@ def handle_subjects_by_level(level, req):
                 chips.append({"text": nama_materi})
 
         if not chips:
-            return jsonify({
-                "fulfillmentText":
-                f"Belum ada pelajaran untuk jenjang {level.upper()}."
-            })
+            return jsonify({"fulfillmentText": f"Belum ada pelajaran untuk jenjang {level.upper()}."})
 
         response = {
-            "fulfillmentMessages": [{
-                "text": {
-                    "text": [
-                        f"Berikut pelajaran untuk jenjang {level.upper()} yang tersedia:"
-                    ]
+            "fulfillmentMessages": [
+                {"text": {"text": [f"Berikut pelajaran untuk jenjang {level.upper()} yang tersedia:"]}},
+                {"payload": {"richContent": [[{"type": "chips", "options": chips}]]}}
+            ],
+            "outputContexts": [
+                {
+                    "name": f"{req['session']}/contexts/pilihjenjang-followup",
+                    "lifespanCount": 5,
+                    "parameters": {
+                        "school_level": level
+                    }
                 }
-            }, {
-                "payload": {
-                    "richContent": [[{
-                        "type": "chips",
-                        "options": chips
-                    }]]
-                }
-            }],
-            "outputContexts": [{
-                "name": f"{req['session']}/contexts/pilihjenjang-followup",
-                "lifespanCount": 5,
-                "parameters": {
-                    "school_level": level
-                }
-            }]
+            ]
         }
 
         return jsonify(response)
     except Exception as e:
         print("Firestore Error:", e)
-        return jsonify({
-            "fulfillmentText":
-            "Terjadi kesalahan saat mengambil data pelajaran."
-        })
+        return jsonify({"fulfillmentText": "Terjadi kesalahan saat mengambil data pelajaran."})
+
